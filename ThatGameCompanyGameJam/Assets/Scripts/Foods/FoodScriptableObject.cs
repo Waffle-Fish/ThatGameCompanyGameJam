da592f67ccf370c45265c71153f8ef4523a41af3
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-05-20 21:50:55 -0700

    implement food + ingredients scriptable objects

---
+++ ThatGameCompanyGameJam/Assets/Scripts/Foods/FoodScriptableObject.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultFood", menuName = "FoodSO", order = 0)]
+public class FoodScriptableObject : ScriptableObject
+{
+    public String DisplayName;
+    public List<IngredientScriptableObject> Ingredients;
+    public Sprite Sprite;
+
+    public float GetTotalCost()
+    {
+        float totalCost = 0;
+        foreach (var ing in Ingredients)
+        {
+            totalCost += ing.Cost;
+        }
+        return totalCost;
+    }
+}
---
+++ ThatGameCompanyGameJam/Assets/Scripts/Foods/Ingredients/IngredientScriptableObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DefaultIngredient", menuName = "IngredientSO", order = 0)]
+public class IngredientScriptableObject : ScriptableObject
+{
+    public float Cost;
+    // public Sprite Sprite;
+    // Add type?
+}
