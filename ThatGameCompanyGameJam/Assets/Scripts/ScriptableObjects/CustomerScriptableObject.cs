5007694225fb02ea06e5ca9944caf953030f1c0c
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-05-22 23:16:07 -0700

    implement customerSO

---
+++ ThatGameCompanyGameJam/Assets/Scripts/ScriptableObjects/CustomerScriptableObject.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "CustomerScriptableObject", menuName = "FoodListScriptableObject", order = 0)]
+public class CustomerScriptableObject : ScriptableObject
+{
+    public Sprite Sprite;
+    public List<FoodScriptableObject> GuranteedFoodOrders;
+    public RandomizedFoodListScriptableObject RandomizedFoodOrders;
+
+    [Min(0)]
+    public int NumRandomFood;
+
+    public List<FoodScriptableObject> FoodOrder { get; private set; } = new();
+    public int FoodOrderTotalCost { get; private set; } = 0;
+
+    public void Awake()
+    {
+        Debug.Log("Awake");
+        InitializeFoodOrder();
+        GetFoodOrderTotal();
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+        FoodOrder.Clear();
+        FoodOrderTotalCost = 0;
+    }
+
+    private void InitializeFoodOrder()
+    {
+        FoodOrder.AddRange(GuranteedFoodOrders);
+        for (int i = 0; i < NumRandomFood; i++)
+        {
+            FoodOrder.Add(RandomizedFoodOrders.RandomlyPickOne());
+        }
+    }
+
+    private void GetFoodOrderTotal()
+    {
+        FoodOrderTotalCost = 0;
+        foreach (var food in FoodOrder)
+        {
+            FoodOrderTotalCost += food.GetTotalCost();
+        }
+    }
+}
