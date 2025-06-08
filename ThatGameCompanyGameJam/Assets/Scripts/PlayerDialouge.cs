dabb88049605ad9d03f2a6b11ed6f07a169d6a41
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-06-08 12:57:38 -0700

    update player dialogue

---
+++ ThatGameCompanyGameJam/Assets/Scripts/PlayerDialouge.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerDialouge : MonoBehaviour
+{
+    TextMeshProUGUI tmp;
+    private void Awake()
+    {
+        tmp = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void PlayDialouge()
+    {
+
+    }
+}
