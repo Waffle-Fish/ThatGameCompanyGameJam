988cf3c5728a3612b2a4a4cbe39218aa34c0dac3
Jhoshua Ampo <ampojhoshua@gmail.com>
2025-07-08 18:41:03 -0700

    Add timeline debugging and marker track to Day 1 scene
    Added a MonoBehaviour to the Day 1 scene for timeline debugging, including logging dialogue track clips in PlayNextDialouge.cs. Introduced a marker track to Day1IntroTimeline.playable and updated scene objects and properties for improved timeline control and debugging.

--- ThatGameCompanyGameJam/Assets/Scripts/PlayNextDialouge.cs
+++ ThatGameCompanyGameJam/Assets/Scripts/PlayNextDialouge.cs
@@ -1,11 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class PlayNextDialouge : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    PlayableDirector playableDirector;
+    TimelineAsset timeline;
+
+    private void Awake()
+    {
+        playableDirector = GetComponent<PlayableDirector>();
+        timeline = playableDirector.playableAsset as TimelineAsset;
+    }
+
     void Start()
     {
-        
+        TrackAsset dialougeTrack = timeline.GetOutputTrack(7); // Track 7
+        var listOfClips = dialougeTrack.GetClips();
+        foreach (var item in listOfClips)
+        {
+            Debug.Log(item);
+        }
     }
 
     // Update is called once per frame
